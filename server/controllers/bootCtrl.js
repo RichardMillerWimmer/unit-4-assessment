8c5e03571ba763aabc2573caac777f4d51a2e722
Richard Miller <richardmmill@gmail.com>
2021-03-12 10:05:56 -0500

    wor on 3.12 at 10:05

---
+++ server/controllers/bootCtrl.js
@@ -0,0 +1,50 @@
+import boots from '../../src/bootsArray'
+
+const collectedBoots = [];
+const collectedId = 0;
+
+
+module.exports = {
+
+    getAvailibleBoots: (req, res) => {
+        res.status(200).send(boots);
+    },
+
+    getCollectedBoots: (req, res) => {
+        res.status(200).send(collectedBoots);
+    },
+
+    postBootToCollection: (req, res) => {
+        const { boot } = req.body;
+
+        boot.id = collectedId;
+        collectedId++;
+
+        collectedBoots.push(boot);
+        res.status(200).send(collectedBoots);
+    },
+
+    addCleanCondition: (req, res) => {
+        const { id } = req.params;
+
+        const cleaningBoots = collectedBoots.find(elem => elem.id === +id);
+        cleaningBoots.cc += 1;
+        res.status(200).send(collectedBoots);
+    },
+
+    addWear: (req, res) => {
+        const { id } = req.params,
+
+        const wearingBoots = collectedBoots.find(elem => elem.id === +id);
+        wearingBoots.wear += 1;
+        res.status(200).send(collectedBoots);
+    },
+
+    deleteBoots: (req, res) => {
+        const { id } = req.params;
+
+        const bootIndex = collectedBoots.findIndex(elem => elem.id === +id);
+        collectedBoots.splice(bootIndex, 1);
+        res.status(200).send(collectedBoots);
+    }
+};
--- src/bootsArray.js
+++ src/bootsArray.js
@@ -9,91 +9,162 @@ const boots = [
     },
     {
         id: 1,
-        model: 8111,
+        model: 8085,
         style: "Iron Ranger",
-        leather: "Amber Harness",
+        leather: "Copper R&T",
         wears: null,
         cc: null
     },
     {
         id: 2,
-        model: 8111,
+        model: 8119,
         style: "Iron Ranger",
-        leather: "Amber Harness",
+        leather: "Oxblood Mesa",
         wears: null,
         cc: null
     },
     {
         id: 3,
-        model: 8111,
+        model: 8086,
         style: "Iron Ranger",
-        leather: "Amber Harness",
+        leather: "Charcol R&T",
         wears: null,
         cc: null
     },
     {
         id: 4,
-        model: 8111,
+        model: 8083,
         style: "Iron Ranger",
-        leather: "Amber Harness",
+        leather: "Hawthorne Muleskinner",
         wears: null,
         cc: null
     },
     {
         id: 5,
-        model: 8111,
+        model: 8084,
         style: "Iron Ranger",
-        leather: "Amber Harness",
+        leather: "Black Harness",
         wears: null,
         cc: null
     },
     {
         id: 6,
-        model: 8111,
-        style: "Iron Ranger",
-        leather: "Amber Harness",
+        model: 8849,
+        style: "Classic Moc",
+        leather: "Black Prairie",
         wears: null,
         cc: null
     },
     {
         id: 7,
-        model: 8111,
-        style: "Iron Ranger",
-        leather: "Amber Harness",
+        model: 8856,
+        style: "Classic Moc",
+        leather: "Oxblood Mesa",
         wears: null,
         cc: null
     },
     {
         id: 8,
-        model: 8111,
-        style: "Iron Ranger",
-        leather: "Amber Harness",
+        model: 8859,
+        style: "Classic Moc",
+        leather: "Navy Portage",
         wears: null,
         cc: null
     },
     {
         id: 9,
-        model: 8111,
-        style: "Iron Ranger",
-        leather: "Amber Harness",
+        model: 1907,
+        style: "Classic Moc",
+        leather: "Copper R&T",
         wears: null,
         cc: null
     },
     {
         id: 10,
-        model: 8111,
-        style: "Iron Ranger",
-        leather: "Amber Harness",
+        model: 875,
+        style: "Classic Moc",
+        leather: "Oro Legacy",
         wears: null,
         cc: null
     },
     {
         id: 11,
-        model: 8111,
-        style: "Iron Ranger",
+        model: 8883,
+        style: "Classic Moc",
+        leather: "Concrete R&T",
+        wears: null,
+        cc: null
+    },
+    {
+        id: 12,
+        model: 8890,
+        style: "Classic Moc",
+        leather: "Charcoal R&T",
+        wears: null,
+        cc: null
+    },
+    {
+        id: 13,
+        model: 8138,
+        style: "Classic Moc",
+        leather: "Briar Oil-Slick",
+        wears: null,
+        cc: null
+    },
+    {
+        id: 14,
+        model: 3345,
+        style: "Blacksmith",
+        leather: "Black Prairie",
+        wears: null,
+        cc: null
+    },
+    {
+        id: 15,
+        model: 3343,
+        style: "Blacksmith",
+        leather: "Copper R&T",
+        wears: null,
+        cc: null
+    },
+    {
+        id: 16,
+        model: 3340,
+        style: "Blacksmith",
+        leather: "Birar Oil-Slick",
+        wears: null,
+        cc: null
+    },
+    {
+        id: 17,
+        model: 3341,
+        style: "Blacksmith",
+        leather: "Charcoal R&T",
+        wears: null,
+        cc: null
+    },
+    {
+        id: 18,
+        model: 8064,
+        style: "Merchant",
         leather: "Amber Harness",
         wears: null,
         cc: null
     },
-
+    {
+        id: 19,
+        model: 8146,
+        style: "Roughneck",
+        leather: "Briar Oil-Slick",
+        wears: null,
+        cc: null
+    },
+    {
+        id: 20,
+        model: 2942,
+        style: "Roughneck",
+        leather: "Copper R&T",
+        wears: null,
+        cc: null
+    }
 ]
